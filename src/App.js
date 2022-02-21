93e0848da6ec5e4c4be6a34bedd985fffc20c6eb
Visconttig <visconttig@gmail.com>
2022-02-21 14:51:49 -0300

    Add 'shop.data' and create 'Shop' page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import { Route } from 'react-router-dom';
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 
 
@@ -8,7 +9,8 @@ import { Route } from 'react-router-dom';
 function App() {
   return (
     <div>
-      <Route exact={true} path={"/"} component={HomePage} />
+      <Route exact={true} path="/" component={HomePage} />
+      <Route exact={true} path="/shop" component={ShopPage} />
     </div>
   );
 }
