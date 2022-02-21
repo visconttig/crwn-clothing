2b1fe02e42a8a0502213009cc4b7e7670b303193
Visconttig <visconttig@gmail.com>
2022-02-21 13:23:12 -0300

    Update MenuItem component to use navigation with 'withRouter' HOC.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import { Route } from 'react-router-dom';
+
+
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact={true} path={"/"} component={HomePage} />
     </div>
   );
 }
