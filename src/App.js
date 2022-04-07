23e5b4d7539a7c8108c83a53b001590f4ad420bd
Visconttig <visconttig@gmail.com>
2022-04-07 12:49:41 -0400

    Fresh start, refactor to use Hooks.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx';
-import { Route, Switch } from 'react-router-dom';
-import ShopPage from "./pages/shop/shop.component.jsx";
-import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
-import Header from "./components/header/header.component.jsx";
+
 
 
 
@@ -11,12 +7,7 @@ import Header from "./components/header/header.component.jsx";
 function App() {
   return (
     <div>
-      <Header />
-      <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-      <Route path="/signin" component={SignInAndSignUp} />
-      </Switch>
+      Hello there!
     </div>
   );
 }
