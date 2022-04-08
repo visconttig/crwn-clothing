98267e1a48aa0fc3a267abed9c0248176bd91d16
Visconttig <visconttig@gmail.com>
2022-04-08 12:15:01 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -6,7 +6,43 @@
 
 const App = () => {
   return (
-    <div>Hello there!</div>
+    <div className="categories-container">
+      <div className="category-container">
+        {/* image here */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* image here */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* image here */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* image here */}
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* image here */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
