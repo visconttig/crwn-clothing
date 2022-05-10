4dbcd27ca128dd5bb76441dbc1da004616f8206e
Visconttig <visconttig@gmail.com>
2022-05-10 18:14:09 -0400

    Add Navigation component, the Logo and some styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Home from "./routes/home/Home.component.jsx";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component.jsx";
 
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Im the SHOP page</h1>
+    </div>
+  );
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={ <Home/> } />
+      <Route path="/" element={ <Navigation/>} >
+      <Route index element={ <Home/> } />
+      <Route path="shop" element={ <Shop/> } />
+      </Route> 
     </Routes>
   );
 };
