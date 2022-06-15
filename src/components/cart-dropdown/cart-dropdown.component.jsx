72876e5263a8268f81ced23f556de83bdb588702
Visconttig <visconttig@gmail.com>
2022-06-15 00:27:26 -0400

    ...

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -9,10 +9,15 @@ const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
     return (
         <div className="cart-dropdown-container">
-            <div className="cart-items"/>
-            { cartItems.map((item) => (
-                <CartItem key={item.id} cartItem={item}/>
-            )) }
+            <div className="cart-items">
+            {cartItems.length 
+                ? (cartItems.map((item) => (
+                    <CartItem key={item.id} cartItem={item} />
+                )))
+                : (
+                    <span className="empty-message">Your cart is empty.</span>
+                )}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     );
