7f88511b0a3dd7ac79d7c1f692e40c9a7c4b47f1
Visconttig <visconttig@gmail.com>
2022-06-20 16:05:42 -0400

    Refactor increase and decrease functionality and structure.

--- src/components/checkout/checkout-item-detail.component.jsx
+++ src/components/checkout/checkout-item-detail.component.jsx
@@ -8,6 +8,8 @@ const CheckoutItemDetail = ({product}) => {
     const { addToCart, removeOrDecreaseFromCart, deleteFromCart } = useContext(CartContext);
 
     const deleteFromCartHandler = () => deleteFromCart(product);
+    const decreaseItemHandler = () => removeOrDecreaseFromCart(product);
+    const increseItemHandler = () => addToCart(product);
     
 
     return (
@@ -16,7 +18,17 @@ const CheckoutItemDetail = ({product}) => {
             <img src={imageUrl} />
         </div>
         <span className="name">{name}</span>       
-        <span className="quantity">{quantity}</span>
+        <span className="quantity">
+            <div className="arrow" onClick={decreaseItemHandler}>
+            &#10094;
+            </div>
+                <span className="value">
+                    {quantity}
+                </span>
+            <div className="arrow" onClick={increseItemHandler}>
+            &#10095;
+            </div>
+        </span>
         <span className="price">{price}</span>
         <div className="remove-button" onClick={deleteFromCartHandler}>&#10005;</div>
     </div>  
