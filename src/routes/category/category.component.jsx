f72680ce1bb3e406517b0a5168ff062c8f39037e
Visconttig <visconttig@gmail.com>
2022-09-17 03:18:08 -0300

    Styled components refactoring -category component.

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,7 +3,7 @@ import { Fragment } from "react";
 import { useState, useEffect } from "react";
 import { CategoriesContext } from "../../contexts/categories.context.jsx";
 import { useParams } from "react-router-dom";
-import "./category.styles.scss";
+import { $CategoryContainer, $CategoryTitle } from "./category.styles.jsx";
 
 import  ProductCard  from "../../components/product-card/product-card.component.jsx";
 
@@ -18,15 +18,15 @@ const Category = () => {
 
     return (
            <Fragment>
-            <h2 className="category-title">{category.toUpperCase()} </h2>
-            <div className="category-container">
+            <$CategoryTitle>{category.toUpperCase()} </$CategoryTitle>
+            <$CategoryContainer>
             {
                 products &&  // programming defensively against null or undefined !!
                 products.map((product) => (
                 <ProductCard key={product.id} product={product} />
             ))
             }
-           </div>
+           </$CategoryContainer>
            </Fragment>
     );
 }
