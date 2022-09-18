c9dec1fe18c46875ff33a3654a3a3ccad2e7d402
Visconttig <visconttig@gmail.com>
2022-09-18 02:11:13 -0300

    Styled components refactoring - Directory-item component.

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,19 +1,19 @@
-import "./directory-item.styles.scss";
+import { $DirectoryItemContainer, $BackgroundImage, $DirectoryItemBody, $H2, $P } from "./directory-item.styles.jsx";
 
 const DirectoryItem = ({category}) => {
     const {imageUrl, title} = category;
     return (
-        <div className="directory-item-container">
-        <div className="background-image"
+        <$DirectoryItemContainer>
+        <$BackgroundImage
           style={{
               backgroundImage: `url(${imageUrl})`
             }}
         />
-        <div className="directory-item-body">
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+        <$DirectoryItemBody>
+          <$H2>{title}</$H2>
+          <$P>Shop Now</$P>
+        </$DirectoryItemBody>
+      </$DirectoryItemContainer>
     );
 }
 
